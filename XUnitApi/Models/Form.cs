b471c804c934d9ce8827347af1b668fc07a05c67
Varun Babu <varun.babu@nestgroup.net>
2023-09-04 12:05:33 +0530

    organised

--- XUnitApi/Models/Form.cs
+++ XUnitApi/Models/Form.cs
@@ -10,82 +10,84 @@ public partial class Form
     [Key]
     public Guid Id { get; set; }
 
+    [JsonIgnore]
     public Guid? RatebookId { get; set; }
 
     public Guid? TableId { get; set; }
-
+    [JsonIgnore]
     public string? AddChangeDeleteFlag { get; set; }
 
     public int? Sequence { get; set; }
-
+    [JsonIgnore]
     public int? SubSequence { get; set; }
 
     public string? Type { get; set; }
 
     public int? MinOccurs { get; set; }
-
+   
     public int? MaxOccurs { get; set; }
-
+    [JsonIgnore]
     public string? Number { get; set; }
 
     public string? Name { get; set; }
-
+    [JsonIgnore]
     public string? Comment { get; set; }
-
+    [JsonIgnore]
     public string? HelpText { get; set; }
-
+    [JsonIgnore]
     public string? Condition { get; set; }
-
+    [JsonIgnore]
     public int? HidePremium { get; set; }
-
+    [JsonIgnore]
     public string? TemplateFile { get; set; }
-
+    [JsonIgnore]
     public int? Hidden { get; set; }
-
+    [JsonIgnore]
     public string? TabCondition { get; set; }
-
+    [JsonIgnore]
     public string? TabResourceName { get; set; }
-
+    [JsonIgnore]
     public string? BtnResAdd { get; set; }
-
+    [JsonIgnore]
     public string? BtnResModify { get; set; }
-
+    [JsonIgnore]
     public string? BtnResDelete { get; set; }
-
+    [JsonIgnore]
     public string? BtnResViewDetail { get; set; }
-
+    [JsonIgnore]
     public string? BtnResRenumber { get; set; }
-
+    [JsonIgnore]
     public string? BtnResView { get; set; }
-
+    [JsonIgnore]
     public string? BtnResCopy { get; set; }
-
+    [JsonIgnore]
     public string? BtnCndAdd { get; set; }
-
+    [JsonIgnore]
     public string? BtnCndModify { get; set; }
-
+    [JsonIgnore]
     public string? BtnCndDelete { get; set; }
-
+    [JsonIgnore]
     public string? BtnCndViewDetail { get; set; }
-
+    [JsonIgnore]
     public string? BtnCndRenumber { get; set; }
-
+    [JsonIgnore]
     public string? BtnCndView { get; set; }
-
+    [JsonIgnore]
     public string? BtnCndCopy { get; set; }
-
+    [JsonIgnore]
     public string? BtnLblAdd { get; set; }
-
+    [JsonIgnore]
     public string? BtnLblModify { get; set; }
-
+    [JsonIgnore]
     public string? BtnLblDelete { get; set; }
-
+    [JsonIgnore]
     public string? BtnLblViewDetail { get; set; }
-
+    [JsonIgnore]
     public string? BtnLblRenumber { get; set; }
-
+    [JsonIgnore]
     public string? BtnLblView { get; set; }
 
+    [JsonIgnore]
     public string? BtnLblCopy { get; set; }
 
     [JsonIgnore]
